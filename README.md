ded1e207ebe511d50f07d47128044da30de418ef
syossan27 <wisdom1027@gmail.com>
2016-01-06 03:51:21 +0900

    implement write Template to README.md

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# torisetsu
+
+Overview
+
+## Description
+
+## Demo
+
+## VS.
+
+## Requirement
+
+## Usage
+
+## Install
+
+## Contribution
+
+## License
+
+[MIT](https://github.com/tcnksm/tool/blob/master/LICENCE)
+
+## Author
+
+[](https://github.com/)
---
+++ torisetsu.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+  "os"
+  "bufio"
+  "github.com/codegangsta/cli"
+)
+
+func main() {
+  app := cli.NewApp()
+  app.Name = "torisetsu"
+  app.Usage = "Write README.md Template"
+  app.Version = "1.0"
+  app.Action = func(c *cli.Context) {
+    file, err := os.OpenFile("README.md", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
+    if err != nil {
+      println("Error")
+      os.Exit(0)
+    }
+    defer file.Close()
+
+    readme_template := []byte(
+      "\nOverview\n\n" +
+      "## Description\n\n" +
+      "## Demo\n\n" +
+      "## VS.\n\n" +
+      "## Requirement\n\n" +
+      "## Usage\n\n" +
+      "## Install\n\n" +
+      "## Contribution\n\n" +
+      "## License\n\n" +
+      "[MIT](https://github.com/tcnksm/tool/blob/master/LICENCE)\n\n" +
+      "## Author\n\n" +
+      "[](https://github.com/)",
+    )
+
+    writer := bufio.NewWriter(file)
+    writer.Write(readme_template)
+    writer.Flush()
+
+    println("Complete write README.md!")
+  }
+  app.Run(os.Args)
+}
