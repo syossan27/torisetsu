86b503a02fa54fc1f53ed6edbbaa082ab798b408
Shintaro Kaneko <kaneshin0120@gmail.com>
2016-01-06 16:38:53 +0900

    Implement an option to print author and format go file with gofmt

--- torisetsu.go
+++ torisetsu.go
@@ -1,44 +1,56 @@
 package main
 
 import (
-  "os"
-  "bufio"
-  "github.com/codegangsta/cli"
+	"bufio"
+	"fmt"
+	"github.com/codegangsta/cli"
+	"os"
+	"strings"
 )
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "torisetsu"
-  app.Usage = "Write README.md Template"
-  app.Version = "1.0"
-  app.Action = func(c *cli.Context) {
-    file, err := os.OpenFile("README.md", os.O_WRONLY|os.O_APPEND, 0600)
-    if err != nil {
-      println("Error: README.md is not exist.")
-      os.Exit(0)
-    }
-    defer file.Close()
+	app := cli.NewApp()
+	app.Name = "torisetsu"
+	app.Usage = "Write README.md Template"
+	app.Version = "1.0"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "author, a",
+			Value: "",
+			Usage: "This flag specifies the author name to print.",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		file, err := os.OpenFile("README.md", os.O_WRONLY|os.O_APPEND, 0600)
+		if err != nil {
+			println("Error: README.md is not exist.")
+			os.Exit(0)
+		}
+		defer file.Close()
 
-    readme_template := []byte(
-      "\nOverview\n\n" +
-      "## Description\n\n" +
-      "## Demo\n\n" +
-      "## VS.\n\n" +
-      "## Requirement\n\n" +
-      "## Usage\n\n" +
-      "## Install\n\n" +
-      "## Contribution\n\n" +
-      "## License\n\n" +
-      "[MIT](https://github.com/tcnksm/tool/blob/master/LICENCE)\n\n" +
-      "## Author\n\n" +
-      "[](https://github.com/)",
-    )
+		author := c.String("author")
 
-    writer := bufio.NewWriter(file)
-    writer.Write(readme_template)
-    writer.Flush()
+		body := []string{
+			"Overview",
+			"## Description",
+			"## Demo",
+			"## VS.",
+			"## Requirement",
+			"## Usage",
+			"## Install",
+			"## Contribution",
+			"## License",
+			"[MIT](https://github.com/tcnksm/tool/blob/master/LICENCE)",
+			"## Author",
+			fmt.Sprintf("[%s](https://github.com/%s)", author, author),
+		}
+		readme_template := []byte("\n" + strings.Join(body, "\n\n"))
 
-    println("Complete add README.md!")
-  }
-  app.Run(os.Args)
+		writer := bufio.NewWriter(file)
+		writer.Write(readme_template)
+		writer.Flush()
+
+		println("Complete add README.md!")
+	}
+	app.Run(os.Args)
 }
